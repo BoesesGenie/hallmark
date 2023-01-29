2cd0341b75593a3ba117841fb0a65a6ebcf57523
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-01-29 15:41:19 +0600

    mobile header

---
+++ src/presentation/components/Layout/components/Navigation/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { ReactComponent as Menu } from './assets/Menu.svg';
+
+const Navigation: FC = () => {
+  return <Menu />;
+};
+
+export default Navigation;
--- src/presentation/components/Layout/index.tsx
+++ src/presentation/components/Layout/index.tsx
@@ -1,5 +1,18 @@
 import React, { FC } from 'react';
+import { ReactComponent as Logo } from './assets/Logo.svg';
+import Navigation from './components/Navigation';
+import './styles.sass';
 
-const Layout: FC = () => <div />;
+const Layout: FC = () => (
+  <div className="layout">
+    <header className="layout__header">
+      <div className="layout__header-top">
+        <Logo />
+        <Navigation />
+      </div>
+      <h1 className="layout__header-title">Doors and Hardware for residential construction industries</h1>
+    </header>
+  </div>
+);
 
 export default Layout;
