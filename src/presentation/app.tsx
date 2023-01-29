fd1d1bf3d8e36a144cc0ba7a8d79ff4784386567
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-01-29 14:30:56 +0600

    eslint config, Layout added, default styles

---
+++ src/presentation/app.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import Layout from './components/Layout';
+
+const App: FC = () => <Layout />;
+
+export default App;
---
+++ src/presentation/components/Layout/index.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const Layout: FC = () => <div />;
+
+export default Layout;
