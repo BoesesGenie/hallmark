a45d5b1590b349bfbd7b83d4da28b8b886c17e18
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-02-03 18:55:24 +0600

    small style fixes

--- src/presentation/components/Layout/index.tsx
+++ src/presentation/components/Layout/index.tsx
@@ -12,6 +12,7 @@ const Layout: FC<{ children?: ReactNode }> = ({ children }) => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    // "document.documentElement.scrollTo" is the magic for React Router Dom v6
     document.documentElement.scrollTo({
       top: 0,
       left: 0,
