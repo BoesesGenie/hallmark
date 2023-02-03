9e41e7820402327601b158b0d6e8cda963b44762
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-02-03 22:37:08 +0600

    PostsList section

---
+++ src/presentation/pages/BlogPage/components/PostsList/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { Typo } from '../../../../ui-kit';
+import image from './assets/image.jpg';
+import './styles.sass';
+
+const PostsList: FC = () => {
+  const title = 'Advice and resources for AAPI Business Owners';
+
+  return (
+    <section className="posts-list">
+      <article>
+        <img className="posts-list__image" src={image} alt={title} />
+        <Typo.BlH1 className="posts-list__title">{title}</Typo.BlH1>
+        <Typo.BlP>How Mielle Organics went viral on social media, and what that virality means for the company initi...</Typo.BlP>
+      </article>
+      <article>
+        <img className="posts-list__image" src={image} alt={title} />
+        <Typo.BlH1 className="posts-list__title">{title}</Typo.BlH1>
+        <Typo.BlP>How Mielle Organics went viral on social media, and what that virality means for the company initi...</Typo.BlP>
+      </article>
+    </section>
+  );
+};
+
+export default PostsList;
--- src/presentation/pages/BlogPage/index.tsx
+++ src/presentation/pages/BlogPage/index.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
 import Layout from '../../components/Layout';
 import MainPost from './components/MainPost';
+import PostsList from './components/PostsList';
 
 const BlogPage: FC = () => (
   <Layout>
     <MainPost />
+    <PostsList />
   </Layout>
 );
 
