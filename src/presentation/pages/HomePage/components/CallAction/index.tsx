7c307e1b2e95ded8a754fd8c6cd7cf718062d92c
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-02-06 02:20:03 +0600

    Call us action desktop width

--- src/presentation/pages/HomePage/components/CallAction/index.tsx
+++ src/presentation/pages/HomePage/components/CallAction/index.tsx
@@ -4,12 +4,14 @@ import ContentAppear from '../../../../components/ContentAppear';
 import './styles.sass';
 
 const CallAction: FC = () => (
-  <section className="call-action">
-    <ContentAppear>
-      <Typo.H2>Ready to discuss <span className="call-action__title-blue">Your project details?</span></Typo.H2>
-      <Button href="#contactUs">Request a call</Button>
-    </ContentAppear>
-  </section>
+  <div className="call-action__wrapper">
+    <section className="call-action">
+      <ContentAppear>
+        <Typo.H2>Ready to discuss <span className="call-action__title-blue">Your project details?</span></Typo.H2>
+        <Button href="#contactUs">Request a call</Button>
+      </ContentAppear>
+    </section>
+  </div>
 );
 
 export default CallAction;
