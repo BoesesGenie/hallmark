d0a1519f595d3bdb3376d16bd395d6050be211e9
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-02-19 17:47:08 +0600

    First step of mobile Showcase animation

--- src/presentation/components/Layout/components/Header/components/Showcase/index.tsx
+++ src/presentation/components/Layout/components/Header/components/Showcase/index.tsx
@@ -1,9 +1,52 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
 import './styles.sass';
 
 const Showcase: FC = () => {
+  const refMobCol1 = useRef<HTMLDivElement>(null);
+  const [mobColWidth, setMobColWidth] = useState<number>(0);
+
+  useEffect(() => {
+    if (!refMobCol1.current?.clientWidth) {
+      return;
+    }
+
+    setMobColWidth(refMobCol1.current?.clientWidth);
+  }, [refMobCol1.current?.clientWidth]);
+
   return (
     <div className="showcase">
+      <div className="showcase__animation">
+        <motion.div
+          className="showcase__animation-col"
+          style={{ backgroundPositionX: `calc(60% + ${mobColWidth}px)` }}
+          ref={refMobCol1}
+          initial={{ transform: 'translateY(50px)' }}
+          animate={{ transform: 'translateY(0px)' }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+        />
+        <motion.div
+          className="showcase__animation-col"
+          style={{ left: '50%' }}
+          initial={{ transform: 'translateY(-50px)' }}
+          animate={{ transform: 'translateY(0px)' }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+        />
+        <motion.div
+          className="showcase__animation-col-bg"
+          style={{ background: 'linear-gradient(20deg, transparent 10%, #fff 10% 90%)' }}
+          initial={{ transform: 'translateY(0px)' }}
+          animate={{ transform: 'translateY(-100vh)' }}
+          transition={{ duration: 0.7 }}
+        />
+        <motion.div
+          className="showcase__animation-col-bg"
+          style={{ left: '50%' }}
+          initial={{ transform: 'translateY(0px)' }}
+          animate={{ transform: 'translateY(100vh)' }}
+          transition={{ duration: 0.7 }}
+        />
+      </div>
       <h1 className="showcase__title">Doors for<br /> residential<br /> industries</h1>
     </div>
   );
