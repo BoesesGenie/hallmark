2ff54511ff5f85d0f2271df37b10c3143fffb359
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-04-17 14:07:18 +0600

    contact us form handler

--- src/presentation/components/Layout/components/Footer/components/ContactUsForm/index.tsx
+++ src/presentation/components/Layout/components/Footer/components/ContactUsForm/index.tsx
@@ -1,16 +1,93 @@
-import React, { FC } from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import { Button, Input, TextArea, Typo } from '../../../../../../ui-kit';
 import './styles.sass';
 
 const ContactUsForm: FC = () => {
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [dataSent, setDataSent] = useState(false);
+
+  const onPhoneChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+  };
+
+  const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const onSubmit = async () => {
+    if (!message || !email) {
+      return;
+    }
+
+    const response = await fetch('/', {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify({
+        phone,
+        email,
+        name,
+        message,
+      }),
+    });
+
+    // TODO -- if you need to parse JSON response
+    // const data = await response.json();
+
+    setDataSent(true);
+
+    window.setTimeout(() => {
+      setDataSent(false);
+    }, 3000);
+  };
+
   return (
     <section className="contact-us-form" id="contactUs">
       <Typo.H2 className="contact-us-form__title">Request a call back</Typo.H2>
-      <Input label="Phone Number" className="contact-us-form__input" />
-      <Input label="Email *" className="contact-us-form__input" />
-      <Input label="Your Name" className="contact-us-form__input" />
-      <TextArea label="Message *" className="contact-us-form__textarea" />
-      <Button className="contact-us-form__button">submit request</Button>
+      <Input
+        label="Phone Number"
+        className="contact-us-form__input"
+        onChange={onPhoneChange}
+      />
+      <Input
+        label="Email *"
+        className="contact-us-form__input"
+        onChange={onEmailChange}
+      />
+      <Input
+        label="Your Name"
+        className="contact-us-form__input"
+        onChange={onNameChange}
+      />
+      <TextArea
+        label="Message *"
+        className="contact-us-form__textarea"
+        onChange={onMessageChange}
+      />
+      <Button
+        className="contact-us-form__button"
+        onClick={onSubmit}
+      >submit request</Button>
+      {dataSent && (
+        <p className="contact-us-form__success-message">Data sent</p>
+      )}
     </section>
   );
 };
