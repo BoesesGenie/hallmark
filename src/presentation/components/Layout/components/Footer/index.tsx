e473928564be6aa3cbb3f935cf38ce15cb8dfba6
Konstantin Kharlamov <k.kharlamow@gmail.com>
2023-05-12 16:12:46 +0600

    green stamp added

--- src/presentation/components/Layout/components/Footer/index.tsx
+++ src/presentation/components/Layout/components/Footer/index.tsx
@@ -3,6 +3,7 @@ import { Typo } from '../../../../ui-kit';
 import ContentAppear from '../../../ContentAppear';
 import stamp from './assets/stamp-colored.svg';
 import stampColored from './assets/stamp-colored.svg';
+import stampGreen from './assets/stamp-green.png';
 import Contacts from './components/Contacts';
 import ContactUsForm from './components/ContactUsForm';
 import './styles.sass';
@@ -17,6 +18,7 @@ const Footer: FC = () => (
         </div>
         <img src={stamp} alt="Hallmark" className="footer__stamp" />
         <img src={stampColored} alt="Hallmark" className="footer__stamp-colored" />
+        <img src={stampGreen} alt="Hallmark" className="footer__stamp-green" />
       </ContentAppear>
       <ContactUsForm />
       <Contacts />
